eac68f2aa5c2f18f45d48255d38cb85a00af5466
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-09-07 21:29:28 +0530

    Completed POMODORO project

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -1,8 +1,12 @@
 #!usr/bin/env python3
 """
-
+This program creates a POMODORO timer with 25 minute long breaks, 5 minutes short break and 15 minute llong break.
+Tkinter module and math module is used.
+Canvas is used to add text to tkinter(using PhotoImage)
+Canvas.text is used to write a text over the image
 """
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,7 +14,8 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-rep = 0
+timer = None
+rep = 1
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -18,61 +23,56 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    count_down(5)
-
+    tk.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    time_label.config(text="Timer")  # >>>> Not working
+    check_label.config(text="")
+    global rep
+    rep = 1
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    min = 9
-    sec = 19
-    count_down(min, sec)
-
-
+    global rep
+    print(rep)
+    if rep % 8 == 0:
+        rep += 1
+        time_label.config(text="Long Break Earned")
+        #long_break_label = Label(text="Long Break Earned", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=RED)
+        #long_break_label.grid(row=1, column=2)
+        count_down(LONG_BREAK_MIN * 60)
+    elif rep % 2 == 0:
+        rep += 1
+        time_label.config(text="Short Break deserved")
+        #short_break_label = Label(text="Short Break deserved", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=PINK)
+        #short_break_label.grid(row=1, column=2)
+        count_down(SHORT_BREAK_MIN * 60)
+    elif rep % 2 == 1:
+        rep += 1
+        time_label.config(text="Work")
+        #work_label = Label(text="Work", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
+        #work_label.grid(row=1, column=2)
+        print(f"work {rep}")
+        count_down(WORK_MIN * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-def count_down(min, sec):
-    canvas.itemconfig(timer_text, text=f"{min}:{sec}")
-    if min >= 10:
+def count_down(count):
+    min = math.floor(count / 60)
+    sec = count % 60
+    if count > 0:
+        global timer
         if sec >= 10:
-            tk.after(1000, count_down, min, sec-1)
-        elif 10 > sec > 0:
+            canvas.itemconfig(timer_text, text=f"{min}:{sec}")
+            timer = tk.after(1000, count_down, count - 1)
+        elif sec < 10:
             canvas.itemconfig(timer_text, text=f"{min}:0{sec}")
-            tk.after(1000, count_down, min, sec - 1)
-        elif sec == 0:
-            canvas.itemconfig(timer_text, text=f"{min}:0{sec}")
-            tk.after(1000, count_down, min-1, 59)
-    elif 10 > min > 0:
-        if sec >= 10:
-            canvas.itemconfig(timer_text, text=f"0{min}:{sec}")
-            tk.after(1000, count_down, min, sec - 1)
-        elif 10 > sec > 0:
-            canvas.itemconfig(timer_text, text=f"0{min}:0{sec}")
-            tk.after(1000, count_down, min, sec - 1)
-        elif sec == 0:
-            canvas.itemconfig(timer_text, text=f"0{min}:0{sec}")
-            tk.after(1000, count_down, min - 1, 59)
-    elif min == 0:
-        if sec >= 10:
-            canvas.itemconfig(timer_text, text=f"0{min}:{sec}")
-            tk.after(1000, count_down, min, sec - 1)
-        elif 10 > sec > 0:
-            canvas.itemconfig(timer_text, text=f"0{min}:0{sec}")
-            tk.after(1000, count_down, min, sec - 1)
-        elif sec == 0:
-            rep += 1
-            if rep == 1 or rep == 3:
-                count_down(4, 59)
-            elif rep == 2 or rep == 4:
-                count_down(9, 59)
-            elif rep == 5:
-                count_down(5, 59)
-
-
-
-
-
-
-
+            timer = tk.after(1000, count_down, count - 1)
+    elif count == 0:
+        start_timer()
+        marks=""
+        work_session = math.floor(rep/2)
+        for _ in range (work_session):
+            marks = "✓"
+        check_label.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -91,18 +91,13 @@ canvas.grid(row=2, column=2)
 
 
 # count_down(5)
-
-def clickbutton():
-    pass
-
-
 start_button = Button(text="Start", command=start_timer, fg=GREEN)
 start_button.grid(row=3, column=1)
 
 reset_button = Button(text="reset", command=reset_timer, fg=GREEN)
 reset_button.grid(row=3, column=3)
 
-check_label = Label(text="✓", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
+check_label = Label(text="", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
 check_label.grid(row=4, column=2)
 
 tk.mainloop()
