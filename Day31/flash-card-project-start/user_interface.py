9f0e785f921116e84713eb12b8e0bfdb5f8e64e1
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-09-18 00:36:06 +0530

    Complete flash project

--- Day31/flash-card-project-start/user_interface.py
+++ Day31/flash-card-project-start/user_interface.py
@@ -13,10 +13,14 @@ FONT_NAME = "Courier"
 BACKGROUND_COLOR = "#B1DDC6"
 timer = None
 select_card = {}
+try:
+    read_words = pd.read_csv("data/word_to_learn.csv")
+    # It creates a list of dictionary items from DataFrame with 2 items in each list.
+except FileNotFoundError:
+    read_words = pd.read_csv("data/french_words.csv")
+finally:
+    word_dict = read_words.to_dict("records")
 
-read_words = pd.read_csv("data/french_words.csv")
-# It creates a list of dictionary items from DataFrame with 2 items in each list.
-word_dict = read_words.to_dict("records")
 
 #----------------------------------Flip Card-------------------------------------------#
 # Creates a english card after 300 second
@@ -26,9 +30,7 @@ def flip_card():
     english_word = select_card["English"]
     #print(f"English: {english_word}")
     canvas.itemconfig(word_text, text=english_word)
-    already_know = messagebox.askyesno(title="Is this word already known", message="Do you want to continue")
-    if already_know:
-        new_word_list = pd.DataFrame({"French":[french_word], "English": [english_word]}
+
 
 #----------------------------------Generate words--------------------------------------#
 # French word appear on flash card when button is pressed.
@@ -43,6 +45,14 @@ def create_words():
     canvas.itemconfig(word_text, text=french_word)
     window.after(3000, flip_card)
 
+
+def word_known():
+    print(len(word_dict))
+    print(select_card)
+    create_words()
+    word_dict.remove(select_card)
+    data_unknown = pd.DataFrame(word_dict)
+    data_unknown.to_csv("data/word_to_learn.csv", index=False)
 
 # -----------------------------------Create UI-----------------------------------------#
 window = Tk()
@@ -60,12 +70,12 @@ word_text = canvas.create_text(400, 300, text="word", font=(FONT_NAME, 60, "bold
 
 # Create button
 wrong_image = PhotoImage(file="images/wrong.png")
-password_button = Button(image=wrong_image, command=create_words, highlightthickness=0.1)
-password_button.grid(row=1, column=0)
+unknown_word = Button(image=wrong_image, command=create_words, highlightthickness=0.1)
+unknown_word.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-password_button = Button(image=right_image, command=create_words, highlightthickness=0.1)
-password_button.grid(row=1, column=1)
+known_word = Button(image=right_image, command=word_known, highlightthickness=0.1)
+known_word.grid(row=1, column=1)
 
 
 window.mainloop()
