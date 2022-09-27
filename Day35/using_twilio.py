6d47cbb7e5b1ce0d3c87e2ddf56931759a0b4b7b
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-09-27 18:25:09 +0530

    Working with API

--- Day35/using_twilio.py
+++ Day35/using_twilio.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC741ffc69c1685f9cca9463023b9bbef4'
-auth_token = '8baaedf946572ae1db351708b4a301a2'
+auth_token = 'd5bc451e2cdea78f3c577c52ba810ff5'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+1xxxx',
-                     to='++1xxxx'
+                     from_='+12545874434',
+                     to='+917406663338'
                  )
 
 print(message.sid)
--- Day35/weather_app.py
+++ Day35/weather_app.py
@@ -8,6 +8,7 @@ take umbrella.
 
 import requests
 from twilio.rest import Client
+import os
 
 api_endpoints = "https://api.openweathermap.org/data/2.5/onecall"
 
@@ -15,7 +16,9 @@ parameters = {
     "lat": 28.613939,
     "lon": 77.209023,
     "exclude": "current,minutely,daily",
-    "appid": "b6bdcd3b831b7aca5fbcf086ef244eb8"
+    # Use environment variables to secure your token, api_keys etc.
+    # Environment variables are stored in env of our systems
+    "appid": os.environ.get("WEATHER_API_KEY")
 }
 
 response = requests.get(url=api_endpoints, params=parameters)
@@ -26,13 +29,13 @@ hourly_data = response.json()["hourly"]
 # Client class takes account_sid and auth_token as parameters to be sent.
 def send_message():
     account_sid = 'AC741ffc69c1685f9cca9463023b9bbef4'
-    auth_token = '8baaedf946572ae1db351708b4a301a2'
+    auth_token = os.environ.get("AUTH_TOKEN")
     client = Client(account_sid, auth_token)
     message = client.messages \
         .create(
         body="Take Umbrella it gonna rain today",
-        from_='+1xxxx',
-        to='+1xxxx'
+        from_='+12XXX',
+        to='+91XXX'
     )
     print(message.status)
 
