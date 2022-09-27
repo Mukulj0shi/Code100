ccb73939868f4838ffd0cf58797168689923de5e
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-09-27 18:26:35 +0530

    Working with API

--- Day35/using_twilio.py
+++ Day35/using_twilio.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC741ffc69c1685f9cca9463023b9bbef4'
-auth_token = 'd5bc451e2cdea78f3c577c52ba810ff5'
+auth_token = "AUTH_TOKEN"
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+12545874434',
-                     to='+917406663338'
+                     from_='+1xxxx',
+                     to='+xxxx'
                  )
 
 print(message.sid)
